224e6b191c0c1497e5f312e592aea5ae26f15cc6
leipeng1223 <leipeng1223@gmail.com>
2022-07-06 21:40:20 +0800

    update 0706

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "colorIcons" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('colorfulIcons.changeIcon', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ColorfulIcons!');
-	});
-
-	context.subscriptions.push(disposable);
+  let disposable = vscode.commands.registerCommand(
+    "colorfulIcons.changeIcon",
+    (uri: vscode.Uri) => {
+	  const filePath = uri.fsPath;
+      // 1.获得当前被右键的文件名和父文件夹(已解决)
+      // 2.读取file icon theme配置文件
+      // 3.向读取的配置中加入新的条目
+      // 4.更新配置文件
+      console.log(filePath);
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
